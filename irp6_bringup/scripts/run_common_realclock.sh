23548b00ac03e3913c68c3d988378700066d8368
Tomasz Winiarski <tomrobotics@gmail.com>
2017-10-25 01:24:49 +0200

    simclock added to sim mode

--- irp6_bringup/scripts/run_common_realclock.sh
+++ irp6_bringup/scripts/run_common_realclock.sh
@@ -4,6 +4,6 @@
 if [[ $(rosnode list | grep diagnostic_aggregator) ]] ; then
 	echo "Common alrady running"
 else
-	roslaunch irp6_bringup irp6-common.launch
+	roslaunch irp6_bringup irp6-common-realclock.launch
 fi
 
---
+++ irp6_bringup/scripts/run_common_simclock.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+if [[ $(rosnode list | grep diagnostic_aggregator) ]] ; then
+	echo "Common alrady running"
+else
+	roslaunch irp6_bringup irp6-common-simclock.launch
+fi
+
