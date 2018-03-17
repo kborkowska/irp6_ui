fa03dc848d6a4a7d605c3ad6dcd7f2ec9bbbdc10
Karolina Borkowska <kborkow@tola.robotyka.ia.pw.edu.pl>
2018-03-17 14:26:12 +0100

    compiling, waiting for a & v max sending

---
+++ irp6_bringup/scripts/limits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from irpos import *
+
+#MAIN
+
+if __name__ == '__main__':
+	
+	irpos = IRPOS("IRpOS", "Irp6p", 6, "irp6p_manager")
+	#irpos.move_to_motor_position([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 10.0)
+	irpos.move_to_joint_position([0.0, -1.5418065817051163,\
+								  0.0, 1.5, 1.57, 0.0], 3.0)
+	#irpos.move_to_joint_position([1.0, -1.5418065817051163,\
+	#							  0.0, 1.5, 1.57, 0.0], 3.0)
+	#irpos.move_to_joint_position([0.0, -1.5418065817051163,\
+	#							  0.0, 1.5, 1.57, 0.0], 3.0)
+	irpos.move_to_joint_position_reserch([1.0, -1.5418065817051163,\
+								  0.0, 1.5, 1.57, 1.0], [1.0, 1.0,\
+								  1.0, 1.0, 1.0, 1.0],[1.0, 1.0,\
+								  1.0, 1.0, 1.0, 1.0])
