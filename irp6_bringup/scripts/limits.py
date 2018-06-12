cc6ce3b58a4f9ce9431b3db645ab1f255ff10a58
Karolina Borkowska <kborkow@tola.robotyka.ia.pw.edu.pl>
2018-06-12 15:24:07 +0200

    after failure

--- irp6_bringup/scripts/limits.py
+++ irp6_bringup/scripts/limits.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from irpos.IRPOS_T import *
+import os
 
 #MAIN
 
@@ -14,7 +15,6 @@ from irpos.IRPOS_T import *
 #upper_limits: [2.9670597284, -0.872664626, 0.6981317008, 1.6057029118, 10.0, 2.93]
 
 if __name__ == '__main__':
-	
 	irpos = IRPOS_T("IRpOS", "Irp6p", 6, "irp6p_manager")
 	#irpos.move_to_motor_position([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 5.0)
 	'''irpos.move_to_joint_position([1.3, -1.5,\
@@ -28,47 +28,44 @@ if __name__ == '__main__':
 	'''irpos.move_to_joint_position([1.0, -1.5418065817051163,\
 								  0.0, 1.5, 1.57, 0.0], 3.0)'''
 	#print irpos.get_joint_position()
-	irpos.move_to_motor_position_trapezoid_velocity(\
+	irpos.move_to_motor_position_trapezoid_duration(\
 								  [400.0, 0.0, 0.0,\
 								   0.0, 0.0, 0.0],\
-								  [25, 35, 35,\
-								   35, 35, 35],\
-								  [10, 20, 20,\
-								   20, 20, 20],\
-								    True, False)
-	'''points = []
-	points.append(JointTrajectoryPoint([-0.10, -1.0, 0.019,\
-									   -1.55, -8.0, -2.74],
+								   True, False)
+	os.system("transferFiles.sh -m 1")
+	points = []
+	'''points.append(JointTrajectoryPoint([400.0, -100.0, -70.0,\
+									   350.0, 400.0, -1000.0],
 									   [0.0, 0.0, 0.0,\
 									   0.0, 0.0, 0.0],
 									   [0.0, 0.0, 0.0,\
-									   0.0, 0.0, 0.0],[], rospy.Duration(3.0)))
-	points.append(JointTrajectoryPoint([2.0, -1.1, 0.0,\
-									   1.0, 4.0, 2.0],
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))
+	points.append(JointTrajectoryPoint([200.0, 100.0, 50.0,\
+									   100.0, 0.0, -500.0],
 									   [0.0, 0.0, 0.0,\
 									   0.0, 0.0, 0.0],
 									   [0.0, 0.0, 0.0,\
-									   0.0, 0.0, 0.0],[], rospy.Duration(3.0)))
-	points.append(JointTrajectoryPoint([-0.1, -1.7, 0.6,\
-									   1.0, 4.0, 2.0],
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))
+	points.append(JointTrajectoryPoint([-200.0, 20.0, 0.0,\
+									   200.0, 0.0, 100.0],
 									   [0.0, 0.0, 0.0,\
 									   0.0, 0.0, 0.0],
 									   [0.0, 0.0, 0.0,\
-									   0.0, 0.0, 0.0],[], rospy.Duration(3.0)))
-	points.append(JointTrajectoryPoint([-0.10063291139240507, -1.5419428654532268, 0.019737556833721442,\
-									   1.1335183568246088, 3.658072916666667, -2.7381185214159984],
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))
+	points.append(JointTrajectoryPoint([100.0, 0.0, -30.0,\
+									   -50.0, 100.0, 0.0],
 									   [0.0, 0.0, 0.0,\
 									   0.0, 0.0, 0.0],
 									   [0.0, 0.0, 0.0,\
-									   0.0, 0.0, 0.0],[], rospy.Duration(3.0)))
-	irpos.move_along_joint_trajectory_trapezoid_velocity(\
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))
+	irpos.move_along_motor_trajectory_trapezoid_velocity(\
 								     points,\
-								     [0.30, 0.30, 0.30,\
-									  0.30, 0.30, 0.30],\
-									 [0.20, 0.20, 0.20,\
-									  0.20, 0.20, 0.20],
-									 False, True)'''
-
+								      [25, 35, 35,\
+									   35, 35, 35],\
+									  [10, 20, 20,\
+									   20, 20, 20],\
+									 True, True)'''
+	#os.system("transferFiles.sh 1 2 3 4 5 6")
 	'''irpos.move_to_joint_position_trapezoid_duration([1.40, -1.0, 0.02,\
 								   -1.55, 1.0, -2.0],\
 								   True, True)'''
@@ -113,3 +110,28 @@ if __name__ == '__main__':
 								  0.2, 1.5, 1.57, 0.0])'''
 	print irpos.get_motor_position()
 	#print irpos.get_motor_position()
+
+	'''points.append(JointTrajectoryPoint([400.0, -100.0, -70.0,\
+									   350.0, 400.0, -1000.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))
+	points.append(JointTrajectoryPoint([200.0, 100.0, 50.0,\
+									   100.0, 0.0, -500.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))
+	points.append(JointTrajectoryPoint([-200.0, 20.0, 0.0,\
+									   200.0, 0.0, 100.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))
+	points.append(JointTrajectoryPoint([100.0, 0.0, -30.0,\
+									   -50.0, 100.0, 0.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],
+									   [0.0, 0.0, 0.0,\
+									   0.0, 0.0, 0.0],[], rospy.Duration(0.0)))'''
